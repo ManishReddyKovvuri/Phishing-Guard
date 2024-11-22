189477565612084ba243c34d897de86cf5bf6944
ManishReddyKovvuri <47208302+ManishReddyKovvuri@users.noreply.github.com>
2024-11-22 15:15:58 -0500

    Uploading source mail.py

---
+++ mail.py
@@ -0,0 +1,51 @@
+import imaplib
+import email
+from email.header import decode_header
+import socket
+from detection import fake_detect
+from GetUrls import parse_email
+
+# Account credentials
+username = "--"
+password = "--"
+imap_server = "imap.gmail.com"
+
+# Connect to the server
+try:
+    # Connect to the Gmail IMAP server
+    mail = imaplib.IMAP4_SSL(imap_server)
+    
+    # Log in to your account
+    mail.login(username, password)
+    
+    # Select the mailbox you want to read from
+    mail.select("inbox")
+    
+    # Search for all emails in the inbox
+    status, messages = mail.search(None, "ALL")
+    
+    # Convert messages to a list of email IDs
+    email_ids = messages[0].split()
+    
+    # Fetch the latest email
+    latest_email_id = email_ids[-1]
+    
+    # Fetch the email by ID
+    status, msg_data = mail.fetch(latest_email_id, "(RFC822)")
+
+
+    
+    # Parse the email content
+    parse_email(msg_data)
+    
+    
+    # Close the connection and logout
+    mail.close()
+    mail.logout()
+
+except imaplib.IMAP4.error as e:
+    print("IMAP error:", e)
+except socket.gaierror as e:
+    print("Socket error:", e)
+except Exception as e:
+    print("Error:", e)
