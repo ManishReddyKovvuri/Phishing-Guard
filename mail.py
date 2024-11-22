7c3cc55202c214ea3478c5c6a86af28335bf3ecc
Raju1998060745 <99238367+Raju1998060745@users.noreply.github.com>
2024-11-22 10:31:33 -0800

    implemented call logic to parse email-body

--- mail.py
+++ mail.py
@@ -2,12 +2,12 @@ import imaplib
 import email
 from email.header import decode_header
 import socket
-from detection import detect
-from GetUrls import getUrls, add_urls_to_dict
+from detection import fake_detect
+from GetUrls import parse_email
 
 # Account credentials
-username = "phishguard.safe@gmail.com"
-password = ""
+username = "manishrk2120@gmail.com"
+password = "kdtn aqyl ofwe tddb"
 imap_server = "imap.gmail.com"
 
 # Connect to the server
@@ -32,56 +32,12 @@ try:
     
     # Fetch the email by ID
     status, msg_data = mail.fetch(latest_email_id, "(RFC822)")
+
+
     
     # Parse the email content
-    for response_part in msg_data:
-        if isinstance(response_part, tuple):
-            msg = email.message_from_bytes(response_part[1])
-            
-            subject, encoding = decode_header(msg["Subject"])[0]
-            if isinstance(subject, bytes):
-                subject = subject.decode(encoding if encoding else "utf-8")
-            from_ = msg.get("From")
-            print("Subject:", subject)
-            print("From:", from_)
+    parse_email(msg_data)
     
-            # If the email message is multipart
-            if msg.is_multipart():
-                for part in msg.walk():
-                    content_type = part.get_content_type()
-                    content_disposition = str(part.get("Content-Disposition"))
-                    if content_type == "text/html":
-                        try:
-                            body = part.get_payload(decode=True).decode()
-                        except:
-                            continue
-                        
-                        # Process HTML content
-                        result = detect(body)
-                        print("from detect (HTML):", result)
-                        urls = getUrls(result)
-                        url_dict = add_urls_to_dict(urls)
-                        print(url_dict)
-                    
-                    elif "attachment" in content_disposition:
-                        filename = part.get_filename()
-                        if filename:
-                            with open(filename, "wb") as f:
-                                f.write(part.get_payload(decode=True))
-            else:
-                content_type = msg.get_content_type()
-                if content_type == "text/html":
-                    try:
-                        body = msg.get_payload(decode=True).decode()
-                    except:
-                        continue
-                    
-                    # Process HTML content
-                    result = detect(body)
-                    print("from detect (HTML non-multipart):", result)
-                    urls = getUrls(result)
-                    url_dict = add_urls_to_dict(urls)
-                    print(url_dict)
     
     # Close the connection and logout
     mail.close()
